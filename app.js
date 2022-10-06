c79120dd02e2711d1ca951480dcd9a5d8c631884
sburchil <symonburchill@gmail.com>
2022-10-06 16:30:02 -0400

    fixed app.get() to app.use()

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/globe', globeRouter);
 
