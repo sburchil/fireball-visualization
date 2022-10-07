2e263e34500825acac4222b517dfd9e832bd686a
sburchil <symonburchill@gmail.com>
2022-10-06 22:45:22 -0400

    updated

--- public/js/extra_fireball.js
+++ public/js/extra_fireball.js
@@ -7,123 +7,119 @@ const sizes = {
 
 let dateText = document.getElementById("date");
 let energyText = document.getElementById("energy");
-let impactData = [];
-
+const initData = setRequestedData(JSON.parse(document.getElementById("responseJson").value));
 let globe = Globe({animateln: true});
 
 $(document).ready(function(){
-    
-    const jsonData = JSON.parse(document.getElementById("responseJson").value);
-    console.log(jsonData);
-
-            let requestedData = jsonData.data;
-            for (let n = 0; n < requestedData.length; n++) {
-                //Get Date & Time
-                let date = requestedData[n][0].split(" ")[0];
-                let time = requestedData[n][0].split(" ")[1];
-
-                //Get Energy
-                let energy = requestedData[n][1];
-
-                //Get Impact Energy
-                let impact_energy = requestedData[n][2];
-
-                //Get Latitude
-                let lat = requestedData[n][3];
-                let latdir = requestedData[n][4];
-                if (latdir == 'S') lat *= -1;
-
-                //Get Longitude
-                let lng = requestedData[n][5];
-                let lngdir = requestedData[n][6];
-                if (lngdir == 'W') lng *= -1;
-
-                //Get Altitude
-                let alt = requestedData[n][7];
-
-                //Get velocity
-                let vel = requestedData[n][8];
-                //Set color and size
-                let color = '';
-                let size = Math.log(1.05 + (energy / 500));
-                if (energy > 300) {
-                    color = '#ff0000';
-                } else if (energy > 150) {
-                    color = '#ff751a';
-                } else if (energy > 75) {
-                    color = '#ffd11a';
-                } else if (energy > 37.5) {
-                    color = '#ffff4d';
-                } else if (energy > 18.25) {
-                    color = '#ffffb3';
-                } else if (energy > 9.125){
-                    color = '#ffffff';
-                } else if (energy > 4.5625){
-                    color = '#b3e6ff';
-                } else if (energy > 2.28125){
-                    color = '#3399ff';
-                } else {
-                    color = '#1111ff';
-                }
-
-                //Create entry
-                let entry = {
-                    date: date,
-                    time: time,
-                    impact_energy: impact_energy,
-                    energy: energy,
-                    lat: lat,
-                    lng: lng,
-                    size: size,
-                    color: color,
-                    alt: alt,
-                    vel: vel
-                };
-
-                //Push entry
-                impactData.push(entry);
-            }
-            initGlobe();
-            globe.onPointClick(point => {
-                console.log(point);
-                destroyGlobe("points");
-                newGlobe();
-            });
-            globe.onLabelClick(label => {
-                globe.controls().autoRotate = false;
-                globe.pointOfView({lat: label.lat, lng: label.lng}, 3000);
-            });
+            initGlobe(initData);
     });
 
-    $("#fireball-params").submit(function (e) {
-        e.preventDefault();
+$("#fireball-params").submit(function (e) {
+    e.preventDefault();
 
-        fire_ajax_submit();
-    });
-
-
+    fire_ajax_submit();
+});
 
 $('#reset').on('click', function(){
     destroyGlobe("labels");
-    globe
-    .pointsData(impactData)
-    .width(sizes.width)
-    .height(sizes.height)
-    .pointsData(impactData)
-    .pointAltitude('size')
-    .pointColor('color');
-    globe.controls().autoRotate = true;
-    globe.controls().autoRotateSpeed = 0.1;
+    globe.pointOfView({altitude: 3}, 2000);
+    pointGlobe(initData);
 })
+
+globe.onPointClick(point => {
+    console.log(point);
+    destroyGlobe("points");
+    labelGlobe([point]);    
+    globe.controls().autoRotate = false;
+    globe.pointOfView({lat: point.lat, lng: point.lng, altitude: 1}, 2000);
+});
+globe.onLabelHover(label => {
+    console.log(label);
+});
 
 window.addEventListener('resize', () =>
 {
     globe.width(window.innerWidth).height(window.innerHeight);
 });
 
-function newGlobe(){
+function setRequestedData(jsonData){
+    let impactData = [];
+    let requestedData = jsonData.data;
+    for (let n = 0; n < requestedData.length; n++) {
+        //Get Date & Time
+        let date = requestedData[n][0].split(" ")[0];
+        let time = requestedData[n][0].split(" ")[1];
+
+        //Get Energy
+        let energy = requestedData[n][1];
+
+        //Get Impact Energy
+        let impact_energy = requestedData[n][2];
+
+        //Get Latitude
+        let lat = requestedData[n][3];
+        let latdir = requestedData[n][4];
+        if (latdir == 'S') lat *= -1;
+
+        //Get Longitude
+        let lng = requestedData[n][5];
+        let lngdir = requestedData[n][6];
+        if (lngdir == 'W') lng *= -1;
+
+        //Get Altitude
+        let alt = requestedData[n][7];
+
+        //Get velocity
+        let vel = requestedData[n][8];
+        //Set color and size
+        let color = '';
+        let size = Math.log(1.05 + (energy / 500));
+        if (energy > 300) {
+            color = '#ff0000';
+        } else if (energy > 150) {
+            color = '#ff751a';
+        } else if (energy > 75) {
+            color = '#ffd11a';
+        } else if (energy > 37.5) {
+            color = '#ffff4d';
+        } else if (energy > 18.25) {
+            color = '#ffffb3';
+        } else if (energy > 9.125){
+            color = '#ffffff';
+        } else if (energy > 4.5625){
+            color = '#b3e6ff';
+        } else if (energy > 2.28125){
+            color = '#3399ff';
+        } else {
+            color = '#1111ff';
+        }
+
+        //Create entry
+        let entry = {
+            date: date,
+            time: time,
+            impact_energy: impact_energy,
+            energy: energy,
+            lat: lat,
+            lng: lng,
+            size: size,
+            color: color,
+            alt: alt,
+            vel: vel
+        };
+
+        //Push entry
+        impactData.push(entry);
+    }
+    return impactData;
+}
+
+function labelGlobe(requestedData){
     globe
-    .labelsData(impactData)
+    .labelsData(requestedData)
+    .labelLabel((el) => {
+        return "Date: " + el.date + ",<br> Time of peak brightness: " + el.time;
+    })
     .labelText('energy')
     .labelSize('size')
     .labelColor('color')
@@ -131,7 +127,19 @@ function newGlobe(){
     .labelResolution(2)
 }
 
-function initGlobe(){
+function pointGlobe(requestedData){
+    globe
+    .pointsData(requestedData)
+    .width(sizes.width)
+    .height(sizes.height)
+    .pointsData(requestedData)
+    .pointAltitude('size')
+    .pointColor('color');
+    globe.controls().autoRotate = true;
+    globe.controls().autoRotateSpeed = 0.1;
+}
+
+function initGlobe(impactData){
     //Create globe
     globe.globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
     .backgroundImageUrl('https://staticdelivery.nexusmods.com/mods/448/images/63-0-1456805047.png')
@@ -164,6 +172,54 @@ function destroyGlobe(choice){
         .labelDotRadius(null)
         .labelResolution(null);
     }
+}
+
+function search() {
+
+    var search = {}
+    search["date-min"] = $("#date-min").val();
+    search["date-max"] = $("#date-max").val();
+    search["energy-min"] = $("#energy-min").val();
+    search["energy-max"] = $("#energy-max").val();
+    search["impact-e-min"] = $("#impact-e-min").val();
+    search["impact-e-max"] = $("#impact-e-max").val();
+    search["limit"] = $("#limit").val();
+
+    var searchParams = new URLSearchParams();
+
+    for(var key in search){
+        if(typeof search[key] !== 'undefined' && search[key] !== ''){
+            console.log(search[key]);
+            searchParams.append(key, search[key]);
+            console.log(searchParams.toString());
+        }
+    }
+    $("#btn-search").prop("disabled", true);
+
+    $.ajax({
+        type: "GET",
+        crossDomain: true,
+        url: "https://ssd-api.jpl.nasa.gov/fireball.api",
+        data: searchParams.toString(),
+        dataType: 'json',
+        contentType: "text/plain",
+        success: function (data) {
+            if(data){
+                console.log("there is data");
+                destroyGlobe("labels");
+                const requestedData = setRequestedData(data);
+                pointGlobe(requestedData);
+            } else {
+                console.log("no data");
+            }
+
+        },
+        error: function (e) {
+            console.log("ERROR: ", e);
+
+        }
+    });
+
 }
 
 
--- routes/globe.js
+++ routes/globe.js
@@ -1,19 +1,30 @@
 var express = require('express');
 var fetch = require('node-fetch');
 var router = express.Router();
+var cors = require('cors');
 
+var allowlist = ['https://fireball.azurewebsites.net', 'http://localhost:3000', 'https://ssd-api.jpl.nasa.gov']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', cors(corsOptionsDelegate), function(req, res, next) {
     res.render('globe', { title: 'Fireball Visualizer' });
 });
 
-router.get('/newglobe', async function(req, res, next) {
+router.get('/newglobe', cors(corsOptionsDelegate), async function(req, res, next) {
     const json = await fetch('https://ssd-api.jpl.nasa.gov/fireball.api', {method: 'GET', headers: {'Content-Type': 'application/json'}});
     const data = await json.json();
     const jsonData = JSON.stringify(data);
     
-    res.render('newglobe', { title: 'Extra example globe', responseJson: jsonData });
+    res.render('newglobe', { title: 'Data Globe', responseJson: jsonData });
 
 });
 
