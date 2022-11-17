feb285297406a399b0298c0ab394c7f0261ed647
sburchil <symonburchill@gmail.com>
2022-11-17 16:23:24 -0500

    update

--- public/js/animation.js
+++ public/js/animation.js
@@ -5,36 +5,44 @@ var backward = $("#backward");
 var backward_fast = $("#backward-fast");
 
 $('#pause-play').click((e) => {
-    if(e.target.class == "fa-solid fa-play" ){
-        e.target.class = "fa-solid fa-pause";
-        speed = 1;
-    } else if(e.target.class == "fa-solid fa-pause"){
-        e.target.class = "fa-solid fa-play";
+    if(e.target.className == "fa-solid fa-play" ){
+        e.target.className = "fa-solid fa-pause";
+        speed = 0.00001;
+    } else if(e.target.className == "fa-solid fa-pause"){
+        e.target.className = "fa-solid fa-play";
         speed = 0;
     }
 });
 
 forward.mousedown(function() {
+    if(speed == 0) return;
     speed = 0.0001
 });
 forward.mouseup(function() {
+    if(speed == 0) return;
     speed = 0.00001
 });
 forward_fast.mousedown(function() {
+    if(speed == 0) return;
     speed = 0.001
 });
 forward_fast.mouseup(function() {
+    if(speed == 0) return;
     speed = 0.00001
 });
 backward.mousedown(function() {
+    if(speed == 0) return;
     speed = -0.0001
 });
 backward.mouseup(function() {
+    if(speed == 0) return;
     speed = 0.00001
 });
 backward_fast.mousedown(function() {
+    if(speed == 0) return;
     speed = -0.001
 });
 backward_fast.mouseup(function() {
+    if(speed == 0) return;
     speed = 0.00001
 });
